377f9426bf7aa09b7bda6e86934396956d2a005f
nessime7 <sara.przebinda@gmail.com>
2023-02-05 00:54:34 +0100

    Initial commit

--- src/main/java/com/PhotomosaicSara/photomosaicImage/Pixel.java
+++ src/main/java/com/PhotomosaicSara/photomosaicImage/Pixel.java
@@ -1,15 +1,15 @@
 package com.PhotomosaicSara.photomosaicImage;
 
 public class Pixel {
-    public int r;
-    public int g;
-    public int b;
+        public int r;
+        public int g;
+        public int b;
 
-    public Pixel(int r, int g, int b) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-    }
+        public Pixel(int r, int g, int b) {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
 
     public static int getRed(int pixel) {
         return (pixel >>> 16) & 0xff;
@@ -24,4 +24,4 @@ public class Pixel {
     }
 
 
-}
+    }
